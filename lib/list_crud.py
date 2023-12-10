37531cec5f5ef1c81292ddd9f528b2f5ebcdfe82
Azani David Pinkney <info@azani.org>
2023-12-10 13:12:36 -0800

    list methods

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,47 @@
 def create_an_empty_list():
-    return None
+    list = []
+    return list
+
 
 def create_a_list():
-    return None
+    list = [1, 2, 3, 4]
+    return list
+
 
 def add_element_to_end_of_list(l, element):
-    return None
+    list = [1, 2, 3, 4]
+    list.append(5)
+    return list
+
 
 def add_element_to_start_of_list(l, element):
-    return None
+    list = [1, 2, 3, 4]
+    list.insert(0, 0)
+    return list
+
 
 def remove_element_from_end_of_list(l):
-    return None
+    list = [1, 2, 3, 4]
+    del list[3]
+    return list
+
 
 def remove_element_from_start_of_list(l):
-    return None
+    list = [1, 2, 3, 4]
+    del list[0]
+    return list
+
 
 def retrieve_first_element_from_list(l):
-    return None
+    list = [1, 2, 3, 4]
+    return list.pop(0)
+
 
 def retrieve_element_from_index(l, index):
-    return None
+    list = [1, 2, 3, 4]
+    return list.pop(index)
+
 
 def retrieve_last_element_from_list(l):
-    return None
+    list = [1, 2, 3, 4]
+    return list.pop()
